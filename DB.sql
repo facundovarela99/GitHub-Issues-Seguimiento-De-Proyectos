8f19bc2a2bcee7072ed7fc340074fb1d69bf1a71
Facundo Varela <facundovarela88@hotmail.com>
2025-12-03 15:15:47 -0300

    Poblando BD
    Poblando la base de datos
    
    base-de-datos #2

--- DB.sql
+++ DB.sql
@@ -11,3 +11,9 @@ CREATE TABLE Proyectos (
     Estado VARCHAR(50)
 );
 
+INSERT INTO proyectos (Nombre, Descripcion, FechaInicio, FechaFin, Estado) VALUES
+('Proyecto A', 'Descripción del Proyecto A', '2023-01-01', '2023-12-31', 'En Progreso'),
+('Proyecto B', 'Descripción del Proyecto B', '2023-02-01', '2023-11-30', 'Planificado'),
+('Proyecto C', 'Descripción del Proyecto C', '2023-03-01', '2023-10-31', 'Completado');
+
+select * from Proyectos;
